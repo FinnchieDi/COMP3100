32bb5d45adc24217f808f4b770f59da4c5c02d96
FinnchieDi <infinnity1203@gmail.com>
2023-03-29 20:28:56 +1100

    Added global variables and constructor

--- Stage1_SocketProgramming_46571582/src/ClientToServer.java
+++ Stage1_SocketProgramming_46571582/src/ClientToServer.java
@@ -1,5 +1,29 @@
 import java.net.*;  
 import java.io.*; 
 public class ClientToServer {
-    
+    Socket sct;
+    DataOutputStream outputStream;
+    BufferedReader inputStream;
+
+
+    //Constructor for defining the Server's IP and Port Address
+    public ClientToServer(String ip, int port) throws Exception{
+        sct = new Socket(ip, port);
+        outputStream = new DataOutputStream(sct.getOutputStream());
+        inputStream = new BufferedReader( new InputStreamReader(sct.getInputStream()));
+    }
+
+    public static void main(String[] args) throws Exception{
+        ClientToServer client = new ClientToServer("192.168.0.10", 50000);
+        //running the server operation
+        client.ClientConnect();
+        //closing the connection between client and server
+        client.sct.close();
+        client.inputStream.close();
+        client.outputStream.close();
+    }
+
+    public void ClientConnect () throws Exception{
+
+    }
 }
