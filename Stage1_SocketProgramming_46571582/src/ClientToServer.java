5a3822d0255dcce0d3b379ba4cc67de6cba0e150
FinnchieDi <infinnity1203@gmail.com>
2023-03-31 22:39:23 +1100

    Adjustments to the scheduling (it actually scheds)

--- Stage1_SocketProgramming_46571582/src/ClientToServer.java
+++ Stage1_SocketProgramming_46571582/src/ClientToServer.java
@@ -26,7 +26,7 @@ public class ClientToServer {
     }
 
     public static void main(String[] args) throws Exception{
-        ClientToServer client = new ClientToServer("10.126.139.160", 50000);
+        ClientToServer client = new ClientToServer("192.168.0.122", 50000);
         //running the server operation
         client.ClientConnect();
         //closing the connection between client and server
@@ -38,28 +38,38 @@ public class ClientToServer {
     public void ClientConnect () throws Exception{
         //Sending initial message
         transmitMsg("HELO");
+        //Server replies with "OK"
         System.out.println("Server responds: " + this.inputStream.readLine());
         
         //transmitting authentication
         String username = System.getProperty("user.name");
         transmitMsg("AUTH " + username);
+        //Server replies with "OK"
         System.out.println("Server responds: " + this.inputStream.readLine());
 
         //Beginning system operations
-        while (lastMsg != "NONE"){
+        while ( jobNum <= 5/*!lastMsg.equals("NONE")*/){
             
             transmitMsg("REDY");
+            //Server replies with "JOBN" or "NONE"
             redyString = this.inputStream.readLine();
-            lastMsg = redyString;
             System.out.println("Server responds: " + redyString);
             String redyPieces[] = redyString.split(" ");
+            lastMsg = redyPieces[0];
+            System.out.println("lastMessage = " + lastMsg);
 
             if (jobNum == 0){
+                
+                System.out.println("Job Type: " + redyPieces[0] + " submitType: " + redyPieces[1] + 
+                                " jobID: " + redyPieces[2] + " estRuntime: " + redyPieces[3] + 
+                                " core: " + redyPieces[4] + " memory: " + redyPieces[5] + " disk: " + redyPieces[6]);
+
                 transmitMsg("GETS All");
                 dataString = this.inputStream.readLine();
                 System.out.println("Server responds: " + dataString);
                 String dataPieces[] = dataString.split(" ");
-                System.out.println("DATA " + dataPieces[0] + " nRecs " + dataPieces[1] + " recLen " + dataPieces[2] + "\n");
+                System.out.println("DATA: " + dataPieces[0] + " nRecs: " + dataPieces[1] + " recLen: " + 
+                                    dataPieces[2] + "\n");
                 totalServers = Integer.parseInt(dataPieces[1]);
 
 
@@ -69,25 +79,32 @@ public class ClientToServer {
                     //Receive each record
                     recordString = this.inputStream.readLine();
                     System.out.println("Server responds: " + recordString);
-                    String recordPieces[] = redyString.split(" ");
+                    String recordPieces[] = recordString.split(" ");
+                    // System.out.println("serverType: " + recordPieces[0] + " serverID: " + recordPieces[1] + 
+                    //                     " serverStatus: " + recordPieces[2] + " currentStartTime: " + 
+                    //                     recordPieces[3] + " core: " + recordPieces[4] + " memory: " + recordPieces[5] + 
+                    //                     " disk: " + recordPieces[6] + " #wJobs: " + recordPieces[7] + " #rJobs: " + recordPieces[8]);
                     //Keep track of the largest server type and the number of servers of that type
                     largestServerType = recordPieces[4];
+
                 }
                 if (largestServerType.equals("16")){
                     largestServerType = "xlarge";
                 }
-                
             }
-
             transmitMsg("OK");
             System.out.println("Server responds: " + this.inputStream.readLine());
 
-            if (redyPieces[0] == "JOBN"){
+            if (redyPieces[0].equals("JOBN")){
                 //Schedule a job
                 transmitMsg("SCHD " + jobNum + " " + largestServerType + " " + serverNum);
+                System.out.println("Scheduled Server: " + largestServerType + ", JobNo " + jobNum + ", ServerID " + serverNum);
                 jobNum += 1;
                 serverNum = (serverNum+1)%totalServers;
                 System.out.println("Server responds: "+ this.inputStream.readLine());
+                
+                transmitMsg("OK");
+                System.out.println("Server responds: " + this.inputStream.readLine());
             }
         }
         //Closing the connection
